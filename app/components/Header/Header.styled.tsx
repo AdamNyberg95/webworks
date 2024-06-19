ef71611f284d1e1944ba7f4bc88dca66d1263a97
adam nyberg <adamnyberg@MBPsomtlhoradam.lan>
2024-06-19 12:39:59 +0300

    chnages to first tests

---
+++ app/components/Header/Header.styled.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  position: fixed;
+  top: 0px;
+  padding: 20px;
+  gap: 16px;
+  background: red;
+  width: 100%;
+`;
---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import { Container } from './Header.styled';
+
+export const Header: React.FC = () => {
+  return <Container>Headers</Container>;
+};
