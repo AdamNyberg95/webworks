ca951968bce99b3b236327f3d764a499ec05bdb0
Saraasso <sara@qte.se>
2024-06-29 08:16:00 +0300

    nav styling

--- app/components/Header/Header.styled.tsx
+++ app/components/Header/Header.styled.tsx
@@ -1,11 +1,63 @@
 import styled from 'styled-components';
+import Link from 'next/link'
 
-export const Container = styled.div`
-  display: flex;
+export const Container = styled.header`
   position: fixed;
-  top: 0px;
-  padding: 20px;
   gap: 16px;
-  background: red;
   width: 100%;
+  height: 73px;
+  display: flex;
+flex-direction: row;
+justify-content: center;
 `;
+
+export const StyledNavList = styled.ul`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+width: 90vw;
+align-items: center;
+max-width: 1400px;
+`;
+
+export const LoggoNavWrapper = styled.div`
+display: flex;
+flex-direction: row;
+`;
+
+
+export const Logga = styled.div`
+display: flex;
+align-items: center;
+padding:  5px;
+`;
+
+export const StyledNavLinkItem = styled.nav`
+display: flex;
+align-items: center;
+padding: 5px 5px;
+
+`;
+
+export const BtnWrapper = styled.div`
+
+`;
+
+export const BtnBooking = styled.button`
+width: 100px;
+height: 40px;
+border-radius: 20px;
+padding: 5px 5px;
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: center;
+border: solid 1px;
+padding: 2px;
+`;
+
+export const BtnTypography = styled.p`
+
+`;
+
+
--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,12 +1,25 @@
 'use client';
 
 import React from 'react';
-import { Container } from './Header.styled';
+import { BtnBooking, BtnTypography, Container, Logga, LoggoNavWrapper, StyledNavLinkItem, StyledNavList} from './Header.styled';
+import logga from '../../assets/icons/logga.ico'
+import Image from 'next/image';
+import { IoIosArrowForward } from "react-icons/io";
 
 export const Header: React.FC = () => {
+const links = ["Våra tjänster", "Om oss", "kontakt"];
+
   return (
-    <div style={{ backgroundColor: 'var(--header-color)', padding: '20px' }}>
-      Header
-    </div>
+    <Container style={{ backgroundColor: 'var(--header-color)' }}>
+      <StyledNavList>
+        <LoggoNavWrapper>
+      <Logga><Image src={logga} alt="logga"  width={72} height={72}/> </Logga>
+        {links.map((link, index) => (
+          <StyledNavLinkItem key={index}>{link}</StyledNavLinkItem>
+        ))}
+      </LoggoNavWrapper>
+      <BtnBooking><BtnTypography>Boka möte </BtnTypography> <IoIosArrowForward /></BtnBooking>
+      </StyledNavList>
+    </Container>
   );
 };
